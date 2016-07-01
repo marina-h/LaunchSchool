927e4ca3d5e7f91902c712acfaa00e10a495f04d
Marina Hoashi <marinahoashi@gmail.com>
2016-07-01 08:18:28 -0400

    Improved mortgage calculator

--- course_101/lesson_2/14_mortgage_calculator.rb
+++ course_101/lesson_2/14_mortgage_calculator.rb
@@ -1,12 +1,10 @@
-# 14. Assignment: Mortgage / Car Loan Calculator
+# Assignment: Mortgage / Car Loan Calculator
 
 # edit value to change language used for messages
 # available language: en = English
 LANGUAGE = 'en'
 
-# messages are found in mortgage_messages.yml
 require 'yaml'
-
 MESSAGES = YAML.load_file('14_mortgage_messages.yml')
 
 def messages(message, lang = 'en')
@@ -19,17 +17,30 @@ def prompt(key, format_hash = "")
 end
 
 def valid_number?(num)
-  # regex for (non-empty) number with an optional period between digits
-  /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+  /\d/.match(num) && /^\d*\.?\d*$/.match(num) && num.to_f > 0
 end
 
-# ask for name and greet user
-prompt("welcome")
+def ask_until_valid_num(input, not_valid_prompt)
+  loop do
+    input = gets.chomp
+    if valid_number?(input)
+      return input.to_f
+    else
+      prompt(not_valid_prompt)
+    end
+  end
+end
+
+def calculate_monthly_payment(l, n, c)
+  monthly_payment = l * (c * (1 + c)**n) / ((1 + c)**n - 1)
+  monthly_payment.round
+end
 
 name = ""
+prompt("welcome")
+
 loop do
   name = gets.chomp
-
   if name.empty?
     prompt("valid_name")
   else
@@ -41,66 +52,26 @@ format_name = { name: name }
 prompt("say_hi", format_name)
 
 loop do
-  # ask for loan amount
-  loan_total = nil
-  loop do
-    prompt("ask_for_loan")
-    loan_total = gets.chomp
-    if valid_number?(loan_total)
-      loan_total = loan_total.to_f
-      break
-    else
-      prompt("not_valid_loan")
-    end
-  end
+  loan_total = annual_rate = loan_years = 0
 
-  # ask for APR in correct format (example: type '5' for 5% interest)
-  annual_rate = nil
-  loop do
-    prompt("ask_for_apr")
-    annual_rate = gets.chomp
-    if valid_number?(annual_rate) # TODO: check for actual percentage (0.05)
-      annual_rate = annual_rate.to_f / 100
-      break
-    else
-      prompt("not_valid_apr")
-    end
-  end
+  prompt("ask_for_loan")
+  loan_total = ask_until_valid_num(loan_total, "not_valid_loan")
 
-  # ask for loan duration in years
-  loan_duration_years = nil
-  loop do
-    prompt("ask_for_duration")
-    loan_duration_years = gets.chomp
-    if valid_number?(loan_duration_years)
-      loan_duration_years = loan_duration_years.to_f
-      break
-    else
-      prompt("not_valid_duration")
-    end
-  end
+  prompt("ask_for_apr")
+  annual_rate = ask_until_valid_num(annual_rate, "not_valid_apr")
 
-  # summarize info given by user and confirm
-  # puts("#{loan_total}, #{apr_annual}, #{loan_duration_years}")
+  prompt("ask_for_duration")
+  loan_years = ask_until_valid_num(loan_years, "not_valid_duration")
 
-  # calculate monthly interest rate
-  monthly_rate = annual_rate / 12
+  monthly_rate = annual_rate / 100 / 12
+  loan_months = loan_years * 12
 
-  # calculate loan duration in months
-  loan_duration_months = loan_duration_years * 12
-
-  # calculate monthly payments
-  # using formula P = L[c(1 + c)**n]/[(1 + c)**n - 1]
-  # from http://www.mtgprofessor.com/formulas.htm
   monthly_payment =
-    loan_total *
-    (monthly_rate * (1 + monthly_rate)**loan_duration_months) /
-    ((1 + monthly_rate)**loan_duration_months - 1)
-  monthly_payment = monthly_payment.round
+    calculate_monthly_payment(loan_total, loan_months, monthly_rate)
 
-  format_summary = { loan_total: loan_total,
+  format_summary = { loan_total: format("%.2f", loan_total),
                      annual_rate: annual_rate,
-                     loan_duration_years: loan_duration_years,
+                     loan_years: loan_years,
                      monthly_payment: monthly_payment }
   prompt("summarize", format_summary)
   prompt("repeat?")
