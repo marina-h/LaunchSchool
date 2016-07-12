26a4826194a978f3b593cdcd1405758475fcf70d
Marina Hoashi <marinahoashi@gmail.com>
2016-07-12 09:13:40 -0400

    Added rock paper scissors from walkthrough

---
+++ course_101/lesson_2/18_rock_paper_scissors.rb
@@ -0,0 +1,49 @@
+#
+
+VALID_CHOICES = %w(rock paper scissors)
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "paper" && second == "rock") ||
+    (first == "scissors" && second == "paper")
+end
+
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("You lost!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+choice = ""
+loop do
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp.downcase
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts("You chose #{choice}; Computer chose: #{computer_choice}")
+
+  display_result(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt("Thank you for playing. Good bye!")
